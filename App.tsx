921283ab99cbc5d7273977fcb1b2462c993b1383
shubhamsinha21 <shubhamsinha07084@gmail.com>
2023-05-15 13:35:45 +0530

    created a function for form validation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
+// form validation
+import * as Yup from 'yup'
+
+const passwordSchema = Yup.object().shape(
+  {
+    passwordLength: Yup.number().min(4, 'Should be min of 4 characters')
+    .max(16, 'Should be a max of 16 characters')
+    .required('Length is required')
+  }
+)
+
 function App() {
+  
+
   return (
    <View>
     <Text style={styles.headerText}>Password Generator</Text>
